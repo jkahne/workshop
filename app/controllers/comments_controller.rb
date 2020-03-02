bdf44e415071664936892758ee33fe6de139ec74
jkahne <jkahne@gmail.com>
2020-03-02 13:48:50 -0500

    v1

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,25 @@
 class CommentsController < ApplicationController
 
   def kurt
-    
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+
+    if @comment.save
+      redirect_to @post
+    else
+      @comments =  @post.comments
+      render 'posts/show'
+
+      # redirect_to @post, notice: @comment.errors.full_messages
+    end
+  end
+
+  private
+
+  def comment_params
+      params.require(:comment).permit(:body)
   end
 end
